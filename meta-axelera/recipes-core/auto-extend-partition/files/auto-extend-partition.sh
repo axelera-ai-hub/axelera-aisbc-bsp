c65d84e81ecc475ebe4c2f7dca46827f607b1dc9
Patrick Barsanti <patrick.barsanti@amarulasolutions.com>
2024-12-16 14:05:12 +0100

    recipes-core: Add auto-extend-partition
    auto-extend-partition is a service, which is automatically ran at boot.
    On first boot, it extends the root partition to occupy all unused space,
    then resizes it.
    
    Change-Id: I1a9fbc950f3b3b97800aa72e1b0821b183cf6829
    Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

---
+++ meta-axelera/recipes-core/auto-extend-partition/files/auto-extend-partition.sh
@@ -0,0 +1,13 @@
+#!bin/sh
+
+MARKER_FILE="/etc/auto-extend-partition.done"
+
+if [ -f "$MARKER_FILE" ]; then
+	exit 0;
+fi
+
+echo "auto-extend-partition: Partition extension process starting..." > /dev/kmsg
+parted -sf /dev/mmcblk0 resizepart 3 100% || { echo "parted command failed in auto-extend-partition script."; exit 1; }
+resize2fs /dev/mmcblk0p3 || { echo "resize2fs command failed in auto-extend-partition script."; exit 1; }
+echo "auto-extend-partition: Partition extended successfully." > /dev/kmsg
+touch "$MARKER_FILE"
