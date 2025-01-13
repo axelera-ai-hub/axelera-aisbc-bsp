axelera-ai-hub/axelera-aisbc-bsp

commit 2143a713fcf8c85f8e6cc2663f15e427ef06cb49

axelera-container: Bump sdk to v1.1.0-rc5
Allow to use version name to download from aws. This make simple
to update everytime. One minor change was to drop subfolder in the url.

Change-Id: I0149e0c16f2305735cc9a302c1d4ba8fca61019b
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

meta-axelera/recipes-core/axelera-container/files/setup_axelera_environment.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 SDK_PATH="voyager-sdk"
+VERSION="v1.1.0-rc5"
 
 # Exit successfully if the voyager-sdk directory exists
 if [ -d "$SDK_PATH" ]; then
@@ -10,7 +11,7 @@ fi
 
 # Download the tar archive
 echo "Downloading archive..."
-curl -O "https://amarula-share.s3.eu-central-1.amazonaws.com/release/v1.1.0-rc2-0-g0ee2fdfd4/sdk-docker-image-build-ubuntu-22.04-arm64/axelera-sdk-ubuntu-2204-arm64.tar" || { echo "Failed to download."; exit 1; }
+curl -O "https://amarula-share.s3.eu-central-1.amazonaws.com/release/$VERSION/axelera-sdk-ubuntu-2204-arm64.tar" || { echo "Failed to download."; exit 1; }
 
 # Load the Docker image from the archive
 echo "Loading Axelera Docker image..."

commit 9086efa2b7427fe08e13257be89052a556419d77

axelera-container: Fix bump to v1.1.0-rc5
The previous commit to bump version to 1.1.0-rc5 was missing some
hardcoded version values which caused errors when launching the script.
Fix this problem by always using a single variable for the version, so
that bumping versions in the future will be much easier in both scripts.

Fixes: 2143a713fcf8c85 ("axelera-container: Bump sdk to v1.1.0-rc5")
Change-Id: I1064c572cc2dded656245fcc147fefbcc4cf7794
Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

meta-axelera/recipes-core/axelera-container/files/setup_axelera_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SDK_PATH="voyager-sdk"
-VERSION="v1.1.0-rc5"
+VERSION="1.1.0-rc5"
 
 # Exit successfully if the voyager-sdk directory exists
 if [ -d "$SDK_PATH" ]; then
@@ -11,7 +11,7 @@ fi
 
 # Download the tar archive
 echo "Downloading archive..."
-return_code=$(curl --write-out %{http_code} -O "https://amarula-share.s3.eu-central-1.amazonaws.com/release/$VERSION/axelera-sdk-ubuntu-2204-arm64.tar")
+return_code=$(curl --write-out %{http_code} -O "https://amarula-share.s3.eu-central-1.amazonaws.com/release/v$VERSION/axelera-sdk-ubuntu-2204-arm64.tar")
 
 if [ "x$return_code" != "x200" ]; then
     echo "Failed to download."
@@ -24,7 +24,7 @@ docker load < axelera-sdk-ubuntu-2204-arm64.tar || { echo "Failed to load Docker
 
 # Create a Docker container from the image
 echo "Creating Docker container from image..."
-CONTAINER_ID=$(docker create axelera-sdk-ubuntu-2204-arm64:1.1.0-rc2) || { echo "Failed to create Docker container"; exit 1; }
+CONTAINER_ID=$(docker create axelera-sdk-ubuntu-2204-arm64:$VERSION) || { echo "Failed to create Docker container"; exit 1; }
 
 # Copy the voyager-sdk from the container
 echo "Copying voyager-sdk from container $CONTAINER_ID..."

meta-axelera/recipes-core/axelera-container/files/start_axelera.py
@@ -7,6 +7,8 @@ import subprocess
 import sys
 import time
 
+version = '1.1.0-rc5'
+
 parser = argparse.ArgumentParser(description='''Axelera Firefly Metis support.''')
 parser.add_argument('-v', '--verbose', action='count', default=0, help='Verbose output')
 subparsers = parser.add_subparsers()
@@ -128,7 +130,7 @@ def start(args):
     '''Start the docker container.'''
     if not os.path.exists('voyager-sdk'):
         print("Extracting voyager-sdk from docker image")
-        container = _run(args, 'docker create axelera-sdk-ubuntu-2204-arm64:1.1.0-rc2').strip()
+        container = _run(args, f'docker create axelera-sdk-ubuntu-2204-arm64:{version}').strip()
         _run(args, f'docker cp {container}:/home/ubuntu/voyager-sdk .', capture=False)
         _run(args, f'docker rm {container}')
         print("Copying this script to voyager-sdk so it is available in the container")
@@ -150,7 +152,7 @@ def start(args):
         '--name=software-platform-voyager-sdk '
         '--entrypoint=/bin/bash '
         '--network=host '
-        'axelera-sdk-ubuntu-2204-arm64:1.1.0-rc2 '
+        f'axelera-sdk-ubuntu-2204-arm64:{version} '
         '-l -c "cd /home/ubuntu/voyager-sdk && . venv/bin/activate && make gst-operators && /bin/bash"',
         capture=False,
         check=False,