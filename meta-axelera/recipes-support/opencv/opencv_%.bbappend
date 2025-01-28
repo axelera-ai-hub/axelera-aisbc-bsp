axelera-ai-hub/axelera-aisbc-bsp

commit aa563940f034b5ff6aeb7318b90205bb231aacde

opencv: enable gstreamer, opencl, and python3 support
Axelera needs Gstreamer and OpenCL support enabled with OpenCV.
Python3 is enabled for testing.

YouTrack-Id: AXELERA-50
Change-Id: I7ce76064cd3d44974140b859a82b2d2c9fc31411
Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>

meta-axelera/recipes-support/opencv/opencv_%.bbappend
@@ -1,0 +1,2 @@
+PACKAGECONFIG:append:antelao = " gstreamer opencl python3"
+PACKAGECONFIG:append:itx-3588j = " gstreamer opencl python3"

commit 7753e0e1756d3e7e6cf56df588947476bb42885b

opencv: Fix enable gstreamer, opencl, and python3 support for Antelao
Enable x board can produce bugs on package configuration. Just drop it for
now because the board supported must have it enable in both cases

Fixes: aa563940f034 ("opencv: enable gstreamer, opencl, and python3 support")
YouTrack-Id: AXELERA-50
Change-Id: Idb6155bda18a0d9e600d676ebe0002a4a4386afd
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

meta-axelera/recipes-support/opencv/opencv_%.bbappend
@@ -1,2 +1,1 @@
-PACKAGECONFIG:append:antelao = " gstreamer opencl python3"
-PACKAGECONFIG:append:itx-3588j = " gstreamer opencl python3"
+PACKAGECONFIG:append = " gstreamer opencl python3"