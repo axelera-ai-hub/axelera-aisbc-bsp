axelera-ai-hub/axelera-aisbc-bsp

commit b351936873c052e205eb74b9e8c3fe355b980380

voyager-image: Add users to docker group
Allow the user to used docker images. We need to check if the privilege
is allowed by the user

YouTrack-Id: AXELERA-48
Change-Id: I2ec88db21cd73accb2bac88e1e657e71337788bc
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

meta-axelera/meta-voyager/recipes-core/images/voyager-image.bb
@@ -6,8 +6,17 @@ SUMMARY = "A simple, minimal image"
 
 inherit extrausers
 
-EXTRA_USERS_PARAMS:append:itx-3588j = "useradd -p '\$5\$n.d2SD190GZItUvJ\$YuFQVXzbgsdN/Ku6ACR6fq1d2M72N9Wg31.0lamahhC' firefly"
-EXTRA_USERS_PARAMS:append:antelao-3588 = "useradd -p '\$5\$L7cOVUOwlfdgicw/\$mzVadqHcAl.NLkCKWPm76.M2SL61Y0bEvQLU3XVKZb7' antelao"
+EXTRA_USERS_PARAMS = "\
+    groupadd docker; \
+"
+
+PASSWORD:itx-3588j = "\$5\$n.d2SD190GZItUvJ\$YuFQVXzbgsdN/Ku6ACR6fq1d2M72N9Wg31.0lamahhC"
+USERNAME:itx-3588j = "firefly"
+PASSWORD:antelao-3588 = "\$5\$L7cOVUOwlfdgicw/\$mzVadqHcAl.NLkCKWPm76.M2SL61Y0bEvQLU3XVKZb7"
+USERNAME:antelao-3588 = "antelao"
+
+EXTRA_USERS_PARAMS += " useradd -p '${PASSWORD}' ${USERNAME}; "
+EXTRA_USERS_PARAMS += " usermod -aG docker ${USERNAME}"
 
 IMAGE_INSTALL = " \
   auto-extend-partition \

commit dcd8048fdea1d8f6fe0fc37ff74c8590ab81598c

voyager-image: Fix adding users to `docker` group
Fix commit adding users to `docker` group.
The missing `;` was causing failed compilation when appending further
instructions to `EXTRA_USERS_PARAMS`.

Fixes: b351936873c0 ("voyager-image: Add users to docker group")
Change-Id: I18b2f0d2d83e4fa2d257bd591d90103d926114a4
Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

meta-axelera/meta-voyager/recipes-core/images/voyager-image.bb
@@ -16,7 +16,7 @@ PASSWORD:antelao-3588 = "\$5\$L7cOVUOwlfdgicw/\$mzVadqHcAl.NLkCKWPm76.M2SL61Y0bE
 USERNAME:antelao-3588 = "antelao"
 
 EXTRA_USERS_PARAMS += " useradd -p '${PASSWORD}' ${USERNAME}; "
-EXTRA_USERS_PARAMS += " usermod -aG docker ${USERNAME}"
+EXTRA_USERS_PARAMS += " usermod -aG docker ${USERNAME}; "
 
 IMAGE_INSTALL = " \
   auto-extend-partition \