axelera-ai-hub/axelera-aisbc-bsp

commit 689c0d2d4306c291b0e971913a7842f134580047

kernel-module: Add workaround for axelera module
The kernel module installation, require even a systemd unit to reboot
the machine if the pci is not discovered

Change-Id: I1de92d789a66a8bc0f13bed738fbdbb8790025ce
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

meta-axelera/recipes-kernel/kernel-modules/kernel-module-axelera_git.bb
@@ -23,13 +23,32 @@ export KERNEL_PATH="${STAGING_KERNEL_DIR}"
 
 inherit auto-patch module
 
-do_install(){
+do_install() {
     install -d ${D}${nonarch_base_libdir}/modules/${KERNEL_VERSION}/kernel/drivers/axelera
     install -m 644 ${S}/*.ko ${D}${nonarch_base_libdir}/modules/${KERNEL_VERSION}/kernel/drivers/axelera/
+
+    install -d ${D}${systemd_system_unitdir}
+    install -d ${D}/usr/local/bin
+
+    install -m 0755 ${WORKDIR}/git/dkms/service_files/check_pcie_device.sh ${D}/usr/local/bin
+    install -m 0644 ${WORKDIR}/git/dkms/service_files/pcie-check.service ${D}${systemd_system_unitdir}
 }
 
 COMPATIBLE_MACHINE = "(itx-3588j|antelao-3588)"
 CLEANBROKEN = "1"
 RPROVIDES:${PN} += "kernel-module-axl-pcie-reset kernel-module-dmabuf-triton-exporter kernel-module-dmabuf-triton-importer kernel-module-metis"
+
+inherit systemd
+
+SYSTEMD_SERVICE:${PN} = "pcie-check.service"
+
+RDEPENDS:${PN} = "bash"
+
+FILES:${PN} += " \
+    /usr/local/bin/check_pcie_device.sh \
+    ${systemd_system_unitdir}/pcie-check.service \
+"
+
+SYSTEMD_AUTO_ENABLE = "enable"
 
 BBCLASSEXTEND =+ "native nativesdk"

commit 8811b7153c81ced12ad5054f902350a99fdc2030

kernel-module-axelera: Fix axelera watchdog service
Service on the official git are wrong for antelao board and even
not needed in the itx board. Create a specific copy for them
and install the service only if needed

Fixes: 689c0d2d4306 ("kernel-module: Add workaround for axelera module")
Change-Id: I411f759934f3c54f6fa5ae5e8228d44aea91f286
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

meta-axelera/recipes-kernel/kernel-modules/kernel-module-axelera-git/antelao-3588/check_pcie_device.sh
@@ -1,0 +1,22 @@
+#!/bin/sh
+
+# File to track whether a reboot has already occurred
+TRACK_FILE="/var/log/pcie_reboot_done"
+
+# Check if the reboot was already performed
+if [ -f "$TRACK_FILE" ]; then
+    echo "Reboot already performed; skipping further checks."
+    # Ensure the next power-on will cause a reboot again
+    rm "$TRACK_FILE"
+    exit 0
+fi
+
+# Check for the PCIe device
+if lspci -nn | grep -qi "1f9d"; then
+    echo "PCIe device with vendor ID 1f9d found. No action required."
+    echo 1 > /sys/bus/pci/devices/0000:01:00.0/triton_restore
+else
+    echo "PCIe device with vendor ID 1f9d not found. Initiating a single reboot."
+    touch "$TRACK_FILE"
+    /sbin/reboot
+fi

meta-axelera/recipes-kernel/kernel-modules/kernel-module-axelera-git/antelao-3588/pcie-check.service
@@ -1,0 +1,12 @@
+[Unit]
+Description=Check for PCIe devices with vendor ID 1f9d and reboot once if not found
+After=network.target
+Requires=network.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/sbin/check_pcie_device.sh
+#RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target

meta-axelera/recipes-kernel/kernel-modules/kernel-module-axelera_git.bb
@@ -13,6 +13,11 @@ LICENSE = "GPL-2.0-only"
 LIC_FILES_CHKSUM = "file://LICENSE;md5=4641e94ec96f98fabc56ff9cc48be14b"
 
 SRC_URI = "${REMOTE_DRIVER};protocol=ssh;branch=release/v1.1.0"
+SRC_URI:append:antelao-3588 = " \
+    file://check_pcie_device.sh \
+    file://pcie-check.service \
+"
+
 SRC_URI[sha256sum] = "ad2598304a8af697d0c335a50a3e5a1ba06c82d9b63ef5f9d3e730b54cf9148a"
 SRCREV = "dcd72e2349965c8726bae3e53155b526ee30d121"
 S = "${WORKDIR}/git/os/driver"
@@ -27,11 +32,14 @@ do_install() {
     install -d ${D}${nonarch_base_libdir}/modules/${KERNEL_VERSION}/kernel/drivers/axelera
     install -m 644 ${S}/*.ko ${D}${nonarch_base_libdir}/modules/${KERNEL_VERSION}/kernel/drivers/axelera/
 
+}
+
+do_install:append:antelao-3588() {
     install -d ${D}${systemd_system_unitdir}
     install -d ${D}/usr/local/bin
 
-    install -m 0755 ${WORKDIR}/git/dkms/service_files/check_pcie_device.sh ${D}/usr/local/bin
-    install -m 0644 ${WORKDIR}/git/dkms/service_files/pcie-check.service ${D}${systemd_system_unitdir}
+    install -m 0755 ${WORKDIR}/check_pcie_device.sh ${D}/usr/local/bin
+    install -m 0644 ${WORKDIR}/pcie-check.service ${D}${systemd_system_unitdir}
 }
 
 COMPATIBLE_MACHINE = "(itx-3588j|antelao-3588)"
@@ -40,15 +48,13 @@ RPROVIDES:${PN} += "kernel-module-axl-pcie-reset kernel-module-dmabuf-triton-exp
 
 inherit systemd
 
-SYSTEMD_SERVICE:${PN} = "pcie-check.service"
+SYSTEMD_SERVICE:${PN}:antelao-3588 = "pcie-check.service"
 
-RDEPENDS:${PN} = "bash"
-
-FILES:${PN} += " \
+FILES:${PN}:antelao-3588 += " \
     /usr/local/bin/check_pcie_device.sh \
     ${systemd_system_unitdir}/pcie-check.service \
 "
 
-SYSTEMD_AUTO_ENABLE = "enable"
+SYSTEMD_AUTO_ENABLE:antelao-3588 = "enable"
 
 BBCLASSEXTEND =+ "native nativesdk"