c55dcb0885a739feed5595bb514cb001bec3decd
Patrick Barsanti <patrick.barsanti@amarulasolutions.com>
2025-03-04 14:11:50 +0100

    axelera-container: Change setup script
    Following permission changes, the start_axelera python script can create
    the voyager-sdk directory within the user home directories.
    Thanks to this, that part of the setup script is not necessary anymore,
    since this can be directly handled by start_axelera.
    
    Additionally, the permission change for weston access is now also
    handled inside start_axelera.
    
    Remove the docker instructions and the permission change.
    Keep this script for internal Amarula comodity, to allow us to handle
    the download and SHA256 checksum of the archive and the docker load.
    
    YouTrack-Id: AXELERA-57
    Change-Id: Ie89e001a691503efaa9af77adf3b6bc25098e435
    Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

--- meta-axelera/recipes-core/axelera-container/files/setup_axelera_environment.sh
+++ meta-axelera/recipes-core/axelera-container/files/setup_axelera_environment.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
 
-SDK_PATH="voyager-sdk"
 VERSION="1.2.0-rc2"
 ARCHIVE_NAME="axelera-sdk-ubuntu-2204-arm64.tar"
 ARCHIVE_SHA256="7a4ca1492b836c8fa39ebdc5f39f1b3125b58d888c8c0049dccc75769eae1058"
-
-# Set up external access to weston
-echo "Setting up external users access to weston..."
-chmod o=x $XDG_RUNTIME_DIR
-chmod o=rwx $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY
-
-# Exit successfully if the voyager-sdk directory exists
-if [ -d "$SDK_PATH" ]; then
-    echo "$SDK_PATH already exists. Setup complete."
-    exit 0
-fi
 
 if [ -f "$ARCHIVE_NAME" ]; then
 	# Archive already present
@@ -34,17 +22,5 @@ fi
 # Load the Docker image from the archive
 echo "Loading Axelera Docker image..."
 docker load < $ARCHIVE_NAME || { echo "Failed to load Docker image."; exit 1; }
-
-# Create a Docker container from the image
-echo "Creating Docker container from image..."
-CONTAINER_ID=$(docker create axelera-sdk-ubuntu-2204-arm64:$VERSION) || { echo "Failed to create Docker container"; exit 1; }
-
-# Copy the voyager-sdk from the container
-echo "Copying voyager-sdk from container $CONTAINER_ID..."
-docker cp $CONTAINER_ID:/home/ubuntu/voyager-sdk . || { echo "Failed to copy voyager-sdk"; exit 1; }
-
-# Remove the container
-echo "Removing Docker container $CONTAINER_ID..."
-docker rm $CONTAINER_ID || { echo "Failed to remove Docker container"; exit 1; }
 
 echo "Setup complete."
