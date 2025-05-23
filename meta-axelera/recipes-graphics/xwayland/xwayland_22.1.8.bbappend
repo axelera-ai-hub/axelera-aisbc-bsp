axelera-ai-hub/axelera-aisbc-bsp

commit ba2d88391dc31015dae9eef33fd0e4c08507cacd

voyager-image-weston: Add xfce4-terminal
The requirement is for the terminal to support 24-bit true color, in
order to correctly display the output of some software suite regarding
the AI accelerator card.

The current terminal emulator, weston-terminal, does not support this
feature.

To fix this, add xfce4-terminal to the Voyager weston image, with all of
its dependencies:
- Modified distro features, as x11 and xwayland are requirements for
  xfce4-terminal.
- Modified weston.ini, such that the icon shown in the top left to open
  the terminal emulator now points to xfce4-terminal instead of
  weston-terminal.

YouTrack-Id: AXELERA-67
Change-Id: I044b20ec13f9dafc98c69801b09edfffdbbe01a2
Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

meta-axelera/meta-voyager/conf/distro/voyager.conf
@@ -28,6 +28,8 @@ DISTRO_FEATURES = " \
   vulkan \
   wayland \
   wifi \
+  x11 \
+  xwayland \
   zeroconf \
 "
 

meta-axelera/meta-voyager/recipes-core/images/voyager-image-weston.bb
@@ -10,4 +10,7 @@ IMAGE_FEATURES:append = " splash hwcodecs weston"
 
 SYSTEMD_DEFAULT_TARGET = "graphical.target"
 
-IMAGE_INSTALL:append = " wayland"
+IMAGE_INSTALL:append = " \
+    wayland \
+    xfce4-terminal \
+"

meta-axelera/recipes-graphics/wayland/files/weston.ini
@@ -33,6 +33,10 @@ background-type=scale-crop
 #icon=/usr/share/icons/gnome/24x24/apps/utilities-terminal.png
 #path=/usr/bin/weston-terminal
 
+[launcher]
+icon=/usr/share/icons/hicolor/32x32/apps/org.xfce.terminal.png
+path=/usr/bin/xfce4-terminal
+
 #[launcher]
 #icon=/usr/share/icons/hicolor/24x24/apps/google-chrome.png
 #path=/usr/bin/google-chrome

commit 6d5339a1c344ab211bc10b3de11c15361f2b8ea7

kas: Add meta-xfce layer and missing dependences
ERROR: Nothing RPROVIDES 'xfce4-terminal'
(but /build/../repo/meta-axelera/meta-voyager/recipes-core/images/voyager-image-weston.bb
RDEPENDS on or otherwise requires it)

Fixes: ba2d88391dc3 ("voyager-image-weston: Add xfce4-terminal")
YouTrack-Id: AXELERA-67
Change-Id: I9ac907b1efd815379e3df154bc0e7501e9687572
Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

kas/include/base.yaml
@@ -28,9 +28,12 @@ repos:
     commit: de8681b4a2a101b99dd2c48d89a7de2ccd9a961f
     layers:
       meta-filesystems:
+      meta-gnome:
       meta-oe:
       meta-python:
       meta-networking:
+      meta-multimedia:
+      meta-xfce:
 
   meta-virtualization:
     url: "https://git.yoctoproject.org/meta-virtualization"

meta-axelera/meta-voyager/conf/distro/voyager.conf
@@ -26,6 +26,7 @@ DISTRO_FEATURES = " \
   usbhost \
   virtualization \
   vulkan \
+  weston-xwayland \
   wayland \
   wifi \
   x11 \

meta-axelera/recipes-graphics/xwayland/xwayland_22.1.8.bbappend
@@ -1,0 +1,1 @@
+DEPENDS += " libtirpc"