1422fc3c3e73be4ffc5bf4fbde3c7469ac7acfec
Ioannis Koltsidas <ioannis.koltsidas@axelera.ai>
2025-07-04 18:29:31 +0200

    setup_axelera_enviroment: Do not hardcode the SDK version number
    Allow the user to download any version of the Axelera SDK container.
    The user specifies the version as a command-line argument to
    setup_axelera_environment.sh. All downloaded containers and associated
    checksum files will be stored in a local directory named after the
    corresponding version. For example, `bash setup_axelera_environment.sh
    1.3.0-rc2` will download the container archive and the corresponding
    checksum file (if found on S3) to directory `v.1.3.0-rc2`. If a
    checksum file is not found (either locally or remotely), the integrity
    check will be skipped.
    
    Change-Id: I365ecd851665ea9ad8915703fc458a511e0b1061
    Signed-off-by: Ioannis Koltsidas <ioannis.koltsidas@axelera.ai>
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

--- meta-axelera/recipes-core/axelera-container/files/setup_axelera_environment.sh
+++ meta-axelera/recipes-core/axelera-container/files/setup_axelera_environment.sh
@@ -1,26 +1,73 @@
 #!/bin/sh
 
-VERSION="1.2.0-rc2"
+# This script is used to setup the Axelera environment.
+# It downloads the Axelera SDK archive (Docker images) and the checksum file,
+# and verifies the integrity of the archive using the checksum file.
+# It then loads the Docker image from the archive.
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <SDK-version>"
+	echo "Example: $0 1.2.5"
+	exit 1
+fi
+
+VERSION=$1
+VERSION_DIR="v${VERSION}"
 ARCHIVE_NAME="axelera-sdk-ubuntu-2204-arm64.tar"
-ARCHIVE_SHA256="7a4ca1492b836c8fa39ebdc5f39f1b3125b58d888c8c0049dccc75769eae1058"
+ARCHIVE_CHECKSUM="${ARCHIVE_NAME}.md5sum"
+
+# Create version-specific directory
+mkdir -p "$VERSION_DIR"
+cd "$VERSION_DIR" || { echo "Failed to create/enter directory $VERSION_DIR"; exit 1; }
+
+download_md5sum() {
+	echo "Downloading checksum file..."
+	return_code=$(curl --write-out %{http_code} -O "https://amarula-share.s3.eu-central-1.amazonaws.com/release/v$VERSION/$ARCHIVE_CHECKSUM")
+
+	if [ "x$return_code" != "x200" ]; then
+		echo "Warning: checksum file not found in remote location."
+		rm -f "$ARCHIVE_CHECKSUM"
+		exit 1
+	fi
+}
+
+verify_md5sum() {
+	if [ -f "$ARCHIVE_CHECKSUM" ]; then
+		echo "Verifying MD5SUM..."
+		if ! md5sum -c "$ARCHIVE_CHECKSUM"; then
+			echo "Error: checksum verification failed. Archive may be corrupted."
+			exit 1
+		fi
+	else
+		echo "Checksum file not found at $ARCHIVE_CHECKSUM. Integrity check will be skipped."
+	fi
+}
 
 if [ -f "$ARCHIVE_NAME" ]; then
-	# Archive already present
-	echo "Archive already downloaded. Checking SHA256..."
-	echo "$ARCHIVE_SHA256  $ARCHIVE_NAME" | sha256sum -c || { echo "Wrong SHA256. Image is corrupted."; exit 1; }
+	# Archive exists, check for MD5SUM
+	echo "Using Docker image stored in directory: $(realpath $PWD)"
+	if [ ! -f "$ARCHIVE_CHECKSUM" ]; then
+		download_md5sum
+	fi
+
+	verify_md5sum
 else
-	# Download the tar archive
-	echo "Downloading archive..."
+	echo "Downloading Docker image into directory: $(realpath $PWD)"
 	return_code=$(curl --write-out %{http_code} -O "https://amarula-share.s3.eu-central-1.amazonaws.com/release/v$VERSION/$ARCHIVE_NAME")
 
 	if [ "x$return_code" != "x200" ]; then
-		echo "Failed to download."
+		echo "Failed to download archive."
+		rm -f "$ARCHIVE_NAME"
 		exit 1
 	fi
+
+	# Download and verify MD5SUM
+	download_md5sum
+	verify_md5sum
 fi
 
 # Load the Docker image from the archive
-echo "Loading Axelera Docker image..."
+echo "Loading Docker image..."
 docker load < $ARCHIVE_NAME || { echo "Failed to load Docker image."; exit 1; }
 
-echo "Setup complete."
+echo "Setup complete. Docker image loaded."
