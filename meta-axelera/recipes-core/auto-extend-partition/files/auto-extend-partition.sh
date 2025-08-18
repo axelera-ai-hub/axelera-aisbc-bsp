932e45c6a54f8f15937bc6e14153b39b666fba24
Patrick Barsanti <patrick.barsanti@amarulasolutions.com>
2025-08-18 17:14:08 +0200

    auto-extend-partition: Remove `parted` command
    After moving from having a single rootfs partition to three different
    partitions (/, data, factory) on the eMMC, the `parted` command has been
    failing to extend mmcblk0p5, giving an error because it is currently
    mounted.
    
    This feature was working correctly when attempting to on-line resize the
    rootfs on mmcblk0p3.
    
    Since practically all users are flashing the board with the Rockchip
    .update.img through `upgrade_tool UF`, which automatically extends the
    last partition to fill the eMMC already, simply remove the `parted`
    command and only leave the `resize2fs`.
    
    Note that this patch effectively breaks proper usage when flashing the
    `.wic` image with `upgrade_tool WL 0`.
    
    Change-Id: Ia20e88a6ab2a2511f763df376a68d43b9ecff296
    Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

--- meta-axelera/recipes-core/auto-extend-partition/files/auto-extend-partition.sh
+++ meta-axelera/recipes-core/auto-extend-partition/files/auto-extend-partition.sh
@@ -7,7 +7,6 @@ if [ -f "$MARKER_FILE" ]; then
 fi
 
 echo "auto-extend-partition: Partition extension process starting..." > /dev/kmsg
-parted -sf /dev/mmcblk0 resizepart 5 100% || { echo "parted command failed in auto-extend-partition script."; exit 1; }
 resize2fs /dev/mmcblk0p5 || { echo "resize2fs command failed in auto-extend-partition script."; exit 1; }
 echo "auto-extend-partition: Partition extended successfully." > /dev/kmsg
 touch "$MARKER_FILE"
