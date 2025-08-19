axelera-ai-hub/axelera-aisbc-bsp

commit fb7b3a4a1c8c6478f363bde3e3df4f66a152b24b

u-boot: use the same u-boot for the itx3588j and antelao
The Antelao uboot works perfectly fine with the itx3588j.
There is no need to maintain separate URI for both.

YouTrack-Id: AXELERA-80
Change-Id: I82fb81ffc3984b1c1b4bcf0561478809325d1db7
Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>

meta-axelera/recipes-bsp/u-boot/u-boot-rockchip.bbappend
@@ -1,7 +1,6 @@
 # Copyright (C) 2019, Fuzhou Rockchip Electronics Co., Ltd
 # Released under the MIT license (see COPYING.MIT for the terms)
-FILESEXTRAPATHS:prepend:antelao-3588 := "${THISDIR}/${PN}:"
-FILESEXTRAPATHS:prepend:itx-3588j := "${THISDIR}/${PN}:"
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
 SOC_FAMILY ?= "rk3588"
 CPU_SERIES = "RK3588"
@@ -9,23 +8,15 @@ PROVIDES += "u-boot"
 
 inherit local-git
 
-SRCREV_rkbin:itx-3588j = "fd46174432fbfdbdd3274bcd94990fd81185ae51"
-SRCREV_uboot:itx-3588j = "f83f9138f588e918effaa200da488f0eb0d5afc1"
+SRCREV_rkbin = "12660714c81be85350a4092542e2ff599aa5adcb"
+SRCREV_uboot = "4cb81ad5558dec446092422d4590d700d9815c7c"
 
-SRC_URI:itx-3588j = " \
-	git://gitlab.com/firefly-linux/u-boot.git;protocol=https;branch="rk3588/firefly";name=uboot; \
-	git://gitlab.com/firefly-linux/rkbin.git;protocol=https;branch="rk3588/firefly";name=rkbin;destsuffix=rkbin; \
-	file://0001-add-itx-3588j_defconfig.patch; \
-	file://0002-add-rockchip-environment-support.patch; \
-"
-
-SRCREV_rkbin:antelao-3588 = "12660714c81be85350a4092542e2ff599aa5adcb"
-SRCREV_uboot:antelao-3588 = "4cb81ad5558dec446092422d4590d700d9815c7c"
-
-SRC_URI:antelao-3588 = " \
+SRC_URI = " \
 	${REMOTE_REPOS_PREFIX}uboot-rockchip.git;protocol=ssh;branch=rk3588;name=uboot; \
 	${REMOTE_REPOS_PREFIX}rk-binary-native.git;protocol=ssh;branch=rk3588;name=rkbin;destsuffix=rkbin; \
-	file://0001-add-rockchip-environment-support.patch; \
+	file://0001-add-itx-3588j_defconfig.patch; \
+	file://0002-add-rockchip-environment-support.patch; \
+	file://0003-use-root-instead-of-uuid-in-kernel-cmdline.patch; \
 "
 
 SRC_URI:append = " ${@bb.utils.contains('CPU_SERIES', 'RK356X', 'file://${CURDIR}/rk356x/0001-add-firefly-rk3566_defconfig.patch', '', d)} \
@@ -33,3 +24,5 @@ ${@bb.utils.contains('CPU_SERIES', 'RK356X', 'file://${CURDIR}/rk356x/0002-remov
 
 # rk3399 not need the patch
 SRC_URI:remove = " ${@bb.utils.contains('SOC_FAMILY', 'rk3399', 'file://${CURDIR}/u-boot/0001-HACK-Support-python3-for-dtoc.patch', '', d)} "
+
+COMPATIBLE_MACHINE = "(itx-3588j|antelao-3588)"

meta-axelera/recipes-bsp/u-boot/u-boot-rockchip/0003-use-root-instead-of-uuid-in-kernel-cmdline.patch
@@ -1,0 +1,31 @@
+From 45daa6901ddccce367c5c0d3eb6d796dbb963f29 Mon Sep 17 00:00:00 2001
+From: Adam Duskett <adam.duskett@amarulasolutions.com>
+Date: Tue, 5 Aug 2025 11:51:20 +0200
+Subject: [PATCH] use root instead of uuid in kernel cmdline
+
+This single line change forces android boot to use root= when booting
+instead of the part uuid. The reason for this change is mender sets
+root=/dev/mmcblk0pX. Without this change, the root= line is ignored,
+causing /dev/mmcblk0p3 to always be the root partition when booting.
+
+Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
+---
+ arch/arm/mach-rockchip/board.c | 2 +-
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+
+diff --git a/arch/arm/mach-rockchip/board.c b/arch/arm/mach-rockchip/board.c
+index e5445af739..a562276412 100644
+--- a/arch/arm/mach-rockchip/board.c
++++ b/arch/arm/mach-rockchip/board.c
+@@ -1377,7 +1377,7 @@ static void bootargs_add_dtb_dtbo(void *fdt, bool verbose)
+ 		 * high priority system to boot and add its UUID
+ 		 * to cmdline. The format is "roo=PARTUUID=xxxx...".
+ 		 */
+-#ifdef CONFIG_ANDROID_AB
++#ifndef CONFIG_ANDROID_AB
+ 		env_update_filter("bootargs", bootargs, "root=");
+ #else
+ 		env_update("bootargs", bootargs);
+-- 
+2.50.1
+

meta-axelera/recipes-bsp/u-boot/u-boot-rockchip/itx-3588j/0002-add-rockchip-environment-support.patch
@@ -1,42 +1,0 @@
-From 286d4c6762b59422895b510e311c15467eef48aa Mon Sep 17 00:00:00 2001
-From: Adam Duskett <adam.duskett@amarulasolutions.com>
-Date: Thu, 24 Jul 2025 10:48:19 +0200
-Subject: [PATCH] add rockchip environment support
-
-Enable environment support for rockchip boards. This is needed
-to read and write bootloader environment variables from userspace
-and is needed for A/B updates and rollbacks.
-
-Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
----
- env/Kconfig                       | 1 +
- include/configs/rockchip-common.h | 3 +++
- 2 files changed, 4 insertions(+)
-
-diff --git a/env/Kconfig b/env/Kconfig
-index 86afc2a2e6..7954049059 100644
---- a/env/Kconfig
-+++ b/env/Kconfig
-@@ -5,6 +5,7 @@ choice
- 	default ENV_IS_IN_MMC if ARCH_SUNXI
- 	default ENV_IS_IN_MMC if ARCH_UNIPHIER
- 	default ENV_IS_IN_MMC if ARCH_EXYNOS4
-+	default ENV_IS_IN_MMC if ARCH_ROCKCHIP
- 	default ENV_IS_IN_MMC if MX6SX || MX7D
- 	default ENV_IS_IN_MMC if TEGRA30 || TEGRA124
- 	default ENV_IS_IN_MMC if TEGRA_ARMV8_COMMON
-diff --git a/include/configs/rockchip-common.h b/include/configs/rockchip-common.h
-index f12af5d6b0..c70e82a9c0 100644
---- a/include/configs/rockchip-common.h
-+++ b/include/configs/rockchip-common.h
-@@ -186,4 +186,7 @@
- #define CONFIG_SYS_AUTOLOAD	"no"
- #define CONFIG_SPL_LOAD_FIT_ADDRESS		0x2000000
- 
-+/*Environment defines*/
-+#define CONFIG_SYS_MMC_ENV_PART 0
-+
- #endif /* _ROCKCHIP_COMMON_H_ */
--- 
-2.50.1
-

commit 00d6d34db129a3294957eba1b53046173a368ea8

recipes-bsp: u-boot-rockchip: Revert partially u-boot merging patch
This patch merge the two u-boot but in the same time add a change.
Revert it and allow antelao to boot again

Fixes: fb7b3a4a1c8c ("u-boot: use the same u-boot for the itx3588j and antelao")
Change-Id: Ic549c380575ca080203bf908d611bb59dacc2292
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

meta-axelera/recipes-bsp/u-boot/u-boot-rockchip.bbappend
@@ -16,7 +16,6 @@ SRC_URI = " \
 	${REMOTE_REPOS_PREFIX}rk-binary-native.git;protocol=ssh;branch=rk3588;name=rkbin;destsuffix=rkbin; \
 	file://0001-add-itx-3588j_defconfig.patch; \
 	file://0002-add-rockchip-environment-support.patch; \
-	file://0003-use-root-instead-of-uuid-in-kernel-cmdline.patch; \
 "
 
 SRC_URI:append = " ${@bb.utils.contains('CPU_SERIES', 'RK356X', 'file://${CURDIR}/rk356x/0001-add-firefly-rk3566_defconfig.patch', '', d)} \

meta-axelera/recipes-bsp/u-boot/u-boot-rockchip/0003-use-root-instead-of-uuid-in-kernel-cmdline.patch
@@ -1,31 +1,0 @@
-From 45daa6901ddccce367c5c0d3eb6d796dbb963f29 Mon Sep 17 00:00:00 2001
-From: Adam Duskett <adam.duskett@amarulasolutions.com>
-Date: Tue, 5 Aug 2025 11:51:20 +0200
-Subject: [PATCH] use root instead of uuid in kernel cmdline
-
-This single line change forces android boot to use root= when booting
-instead of the part uuid. The reason for this change is mender sets
-root=/dev/mmcblk0pX. Without this change, the root= line is ignored,
-causing /dev/mmcblk0p3 to always be the root partition when booting.
-
-Signed-off-by: Adam Duskett <adam.duskett@amarulasolutions.com>
----
- arch/arm/mach-rockchip/board.c | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/arch/arm/mach-rockchip/board.c b/arch/arm/mach-rockchip/board.c
-index e5445af739..a562276412 100644
---- a/arch/arm/mach-rockchip/board.c
-+++ b/arch/arm/mach-rockchip/board.c
-@@ -1377,7 +1377,7 @@ static void bootargs_add_dtb_dtbo(void *fdt, bool verbose)
- 		 * high priority system to boot and add its UUID
- 		 * to cmdline. The format is "roo=PARTUUID=xxxx...".
- 		 */
--#ifdef CONFIG_ANDROID_AB
-+#ifndef CONFIG_ANDROID_AB
- 		env_update_filter("bootargs", bootargs, "root=");
- #else
- 		env_update("bootargs", bootargs);
--- 
-2.50.1
-