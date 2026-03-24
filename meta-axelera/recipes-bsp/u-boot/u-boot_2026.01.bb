axelera-ai-hub/axelera-aisbc-bsp

commit c1758cd39b54605f7bed7b130905bb5dd43dd08d

meta-axelera: Add mainline u-boot 2026.01
Add simple recipe for a recent mainline u-boot version and, as a
temporary solution, patch it to add Antelao support.

YouTrack-Id: AXELERA-183
Change-Id: I6caf47ef178843fb14f8dfc2f9f551a58c9c0f46
Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

meta-axelera/recipes-bsp/u-boot/u-boot_2026.01.bb
@@ -1,0 +1,21 @@
+# Simple recipe for using mainline U-Boot
+
+require recipes-bsp/u-boot/u-boot-common.inc
+require recipes-bsp/u-boot/u-boot.inc
+
+# Rewrite SRC_URI so we don't download the CVE patches: we fetch a more recent
+# version were they have already been applied.
+SRC_URI = " \
+    git://source.denx.de/u-boot/u-boot.git;protocol=https;branch=master \
+    file://0001-HACK-Add-antelao-support-in-u-boot.patch \
+"
+
+# v2026.01
+SRCREV = "127a42c7257a6ffbbd1575ed1cbaa8f5408a44b3"
+LIC_FILES_CHKSUM = "file://Licenses/README;md5=2ca5f2c35c8cc335f0a19756634782f1"
+
+DEPENDS += "\
+    gnutls-native \
+    python3-pyelftools-native \
+    util-linux-native \
+"

commit 391d8893b071d94b1705007449af4559dc5b838c

Fix the naming of U-Boot and kernel patches during submission
YouTrack-Id: AXELERA-183
Fixes: c1758cd39b5 ("meta-axelera: Add mainline u-boot 2026.01")
Fixes: c35785da2f9 ("meta-axelera: Add linux-axelera-mainline")
Change-Id: I96a0d052304121536b831e6f99f8a962c62f2e72
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

meta-axelera/recipes-bsp/u-boot/u-boot_2026.01.bb
@@ -7,7 +7,7 @@ require recipes-bsp/u-boot/u-boot.inc
 # version were they have already been applied.
 SRC_URI = " \
     git://source.denx.de/u-boot/u-boot.git;protocol=https;branch=master \
-    file://0001-HACK-Add-antelao-support-in-u-boot.patch \
+    file://0001-Add-antelao-support-in-u-boot.patch \
 "
 
 # v2026.01

meta-axelera/recipes-kernel/linux/linux-axelera-mainline_6.19.9.bb
@@ -3,5 +3,5 @@ require linux-axelera-mainline.inc
 SRC_URI[sha256sum] = "c16068a3af12e3943dee3b1eef57ca70229c069128bfa1184fb3f48b219d55bf"
 
 SRC_URI += "\
-    file://0001-HACK-Add-antelao-support-to-linux.patch \
+    file://0001-Add-antelao-support-to-linux.patch \
 "