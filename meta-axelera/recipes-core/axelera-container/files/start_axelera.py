axelera-ai-hub/axelera-aisbc-bsp

commit e7ade42223cc69da58fcddb59ef8d2661a048b35

meta-axelera: axelera-container: Mount libraries for panthor
After the backport and move to panthor/panfrost drivers for the Mali
GPU, it is required to update the directories and the devices mounted to
the axelera docker container in order for GPU acceleration and clinfo to
work correctly.

Update them for this purpose, and remove the mali references as we do
not have the Rockchip driver anymore.

YouTrack-Id: AXELERA-198
Change-Id: I02f58177658c5d9d4c959d191a6d696117c5cdfc
Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

meta-axelera/recipes-core/axelera-container/files/start_axelera.py
@@ -530,11 +530,29 @@ def start(args):
     docker_cmd = (
         f"docker run {docker_param} --privileged "
         "-v $(pwd)/shared:/home/ubuntu/shared/ "
-        "-v /usr/lib/libmali-hook.so.1:/usr/lib/libmali-hook.so.1 "
-        "-v /usr/lib/libmali.so.1:/usr/lib/libmali.so.1 "
+        "-v /usr/lib/dri:/usr/lib/aarch64-linux-gnu/dri "
+        "-v /usr/lib/libgallium-26.0.3.so:/usr/lib/aarch64-linux-gnu/libgallium-26.0.3.so "
+        "-v /usr/lib/libEGL.so.1:/usr/lib/aarch64-linux-gnu/libEGL.so.1 "
+        "-v /usr/lib/libgbm.so.1:/usr/lib/aarch64-linux-gnu/libgbm.so.1 "
+        "-v /usr/lib/libGL.so.1:/usr/lib/aarch64-linux-gnu/libGL.so.1 "
+        "-v /usr/lib/libdrm.so.2:/usr/lib/aarch64-linux-gnu/libdrm.so.2 "
+        "-v /usr/share/vulkan:/usr/share/vulkan "
+        "-v /usr/lib/libvulkan.so.1:/usr/lib/aarch64-linux-gnu/libvulkan.so.1 "
+        "-v /usr/lib/libX11.so.6:/usr/lib/aarch64-linux-gnu/libX11.so.6 "
+        "-v /usr/lib/libX11-xcb.so.1:/usr/lib/aarch64-linux-gnu/libX11-xcb.so.1 "
+        "-v /usr/lib/libwayland-client.so.0:/usr/lib/aarch64-linux-gnu/libwayland-client.so.0 "
+        "-v /usr/lib/libLLVM.so.21.1:/usr/lib/aarch64-linux-gnu/libLLVM.so.21.1 "
+        "-v /usr/lib/libLLVMSPIRVLib.so.21.1:/usr/lib/aarch64-linux-gnu/libLLVMSPIRVLib.so.21.1 "
+        "-v /usr/lib/libSPIRV-Tools.so:/usr/lib/aarch64-linux-gnu/libSPIRV-Tools.so "
+        "-v /usr/lib/libSPIRV-Tools-shared.so:/usr/lib/aarch64-linux-gnu/libSPIRV-Tools-shared.so "
+        "-v /usr/lib/libSPIRV-Tools-opt.so:/usr/lib/aarch64-linux-gnu/libSPIRV-Tools-opt.so "
+        "-v /usr/lib/libSPIRV-Tools-link.so:/usr/lib/aarch64-linux-gnu/libSPIRV-Tools-link.so "
+        "-v /usr/lib/libclang-cpp.so.21.1:/usr/lib/aarch64-linux-gnu/libclang-cpp.so.21.1 "
+        "-v /usr/lib/libGLESv2.so.2:/usr/lib/aarch64-linux-gnu/libGLESv2.so.2 "
+        "-v /usr/share/clc/spirv64-mesa3d-.spv:/usr/share/clc/spirv64-mesa3d-.spv "
         "-v /etc/OpenCL:/etc/OpenCL "
-        "-v /data/sdk/container/volumes/usr/lib/aarch64-linux-gnu/mali:/usr/lib/aarch64-linux-gnu/mali:ro "
-        "-v /usr/lib/libMaliOpenCL.so.1:/usr/lib/libMaliOpenCL.so.1 "
+        "-v /usr/lib/libOpenCL.so.1:/usr/lib/aarch64-linux-gnu/libOpenCL.so.1 "
+        "-v /usr/lib/libRusticlOpenCL.so.1:/usr/lib/aarch64-linux-gnu/libRusticlOpenCL.so.1 "
         f"-v /usr/lib/gstreamer-1.0/:/opt/axelera/runtime-{args.version}-1/lib/gstreamer-1.0 "
         "-v /usr/lib/librockchip_mpp.so.1:/usr/lib/librockchip_mpp.so.1 "
         "-v /usr/lib/librga.so.2:/usr/lib/librga.so.2 "
@@ -546,7 +564,10 @@ def start(args):
         "-v /usr/lib/libavutil.so.58:/usr/lib/libavutil.so.58 "
         "-v /usr/lib/libswscale.so.7:/usr/lib/libswscale.so.7 "
         "-v /usr/lib/libswresample.so.4:/usr/lib/libswresample.so.4 "
-        "--device=/dev/mali0 "
+        "--device=/dev/dri "
+        "-e RUSTICL_ENABLE=panthor "
+        "-e MESA_GL_VERSION_OVERRIDE=3.3 "
+        "-e MESA_GLSL_VERSION_OVERRIDE=330 "
         f"{display_config['display_args']} "
         f"--name={args.container_name} "
         "--entrypoint=/bin/bash "

meta-axelera/recipes-core/udev-conf-axelera/files/72-triton.rules
@@ -3,6 +3,5 @@ SUBSYSTEM=="misc", DEVPATH=="/devices/virtual/misc/dmabuf_triton_*", GROUP="axel
 SUBSYSTEM=="metis", DEVPATH=="*/metis/metis*", GROUP="axelera", MODE="0660"
 SUBSYSTEM=="dma_heap", DEVPATH=="/devices/virtual/dma_heap/*", GROUP="axelera", MODE="0660"
 SUBSYSTEM=="mpp_class", DEVPATH=="/devices/platform/mpp-srv/mpp_class/mpp_service", GROUP="axelera", MODE="0660"
-SUBSYSTEM=="misc", KERNEL=="mali0", GROUP="axelera", MODE="0660"
 SUBSYSTEM=="devfreq", DEVPATH=="*/fb000000.gpu", ATTR{governor}="performance"
 ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="05e3", ATTRS{idProduct}=="0620", ATTR{power/control}="on"

commit 176c00fb57d228599dcbe768dbd08e84df569984

meta-axelera: axelera-container: Missing library inside the container
gstreamer require to load the vulkan library or it rises a warning

YouTrack-Id: AXELERA-198
Fixes: e7ade42223cc ("meta-axelera: axelera-container: Mount libraries for panthor")
Change-Id: Ic76cade7785dd30c8c0c720dc1c011ea80edc247
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

meta-axelera/recipes-core/axelera-container/files/start_axelera.py
@@ -553,6 +553,7 @@ def start(args):
         "-v /etc/OpenCL:/etc/OpenCL "
         "-v /usr/lib/libOpenCL.so.1:/usr/lib/aarch64-linux-gnu/libOpenCL.so.1 "
         "-v /usr/lib/libRusticlOpenCL.so.1:/usr/lib/aarch64-linux-gnu/libRusticlOpenCL.so.1 "
+        "-v /usr/lib/libgstvulkan-1.0.so.0:/usr/lib/aarch64-linux-gnu/libgstvulkan-1.0.so.0 "
         f"-v /usr/lib/gstreamer-1.0/:/opt/axelera/runtime-{args.version}-1/lib/gstreamer-1.0 "
         "-v /usr/lib/librockchip_mpp.so.1:/usr/lib/librockchip_mpp.so.1 "
         "-v /usr/lib/librga.so.2:/usr/lib/librga.so.2 "